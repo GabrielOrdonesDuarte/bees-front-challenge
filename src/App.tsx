75355b883468a65e091b181f9d180c0320d4d4c7
GabrielOrdonesDuarte <gabriel.duarte@korok.app>
2022-04-15 01:25:15 -0300

    feat(hook): create User context provider
    This provider will be use to save and provide the user name and the authenticated boolean

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import { UserProvider } from "contexts/user";
 import theme from "styles/theme";
+import GlobalStyles from "styles/global";
 
 const App = () => {
    return (
       <ThemeProvider theme={theme}>
-         <div className="app">
-            <h1>App.tsx</h1>
-         </div>
+         <UserProvider>
+            <GlobalStyles />
+            <div className="app">
+               <h1>App.tsx</h1>
+            </div>
+         </UserProvider>
       </ThemeProvider>
    );
 };
---
+++ src/contexts/user.tsx
@@ -0,0 +1,64 @@
+import React, { useState, createContext, useContext, useEffect } from "react";
+import { getStorageItem, setStorageItem } from "utils/localStorage/index";
+
+const STORAGE_KEY = "fullName";
+
+export interface UserContextInterface {
+   userName: string;
+   isAuthenticated: boolean;
+   addUser: (name: string) => void;
+   logout: () => void;
+}
+
+export const UserContextDefaultValues = {
+   userName: "",
+   isAuthenticated: false,
+   addUser: () => null,
+   logout: () => null,
+};
+
+type UserProviderProps = {
+   children: React.ReactNode;
+};
+
+export const UserContext = createContext<UserContextInterface>(
+   UserContextDefaultValues
+);
+
+export const UserProvider = ({ children }: UserProviderProps) => {
+   const [userName, setUserName] = useState("");
+   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+   useEffect(() => {
+      const data = getStorageItem(STORAGE_KEY)
+      
+      if (data) {
+         setUserName(data);
+         setIsAuthenticated(true);
+      } else {
+         setIsAuthenticated(false);
+      }
+   }, [])
+
+   const addUser = (name: string) => {
+      setUserName(name);
+      setIsAuthenticated(true);
+      setStorageItem(STORAGE_KEY, name);
+   };
+
+   const logout = () => {
+      setUserName("");
+      setIsAuthenticated(false);
+      window.localStorage.removeItem(`BeesChallenge_${STORAGE_KEY}`);
+   };
+
+   return (
+      <UserContext.Provider
+         value={{ isAuthenticated, userName, addUser, logout }}
+      >
+         {children}
+      </UserContext.Provider>
+   );
+};
+
+export const useUser = () => useContext(UserContext);
