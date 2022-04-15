2e3f49d18c7005579c668d1851720fe2e7c2812f
GabrielOrdonesDuarte <gabriel.duarte@korok.app>
2022-04-15 19:13:43 -0300

    feat(toastify): create toastify notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { ThemeProvider } from 'styled-components'
+import { ToastContainer } from 'react-toastify'
+
 import Router from './router'
+import { UserProvider } from 'contexts/user'
+
 import theme from './styles/theme'
 import GlobalStyles from 'styles/global'
-import { ThemeProvider } from 'styled-components'
-import { UserProvider } from 'contexts/user'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <UserProvider>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <GlobalStyles />
         <Router />
       </UserProvider>
