5c7f701d4086629ad1a9e5982c3af6c7aadb5859
GabrielOrdonesDuarte <gabriel.duarte@korok.app>
2022-04-16 16:18:35 -0300

    test(textfield): add TextField component tests

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -4,25 +4,22 @@ import userEvent from '@testing-library/user-event'
 import TextField from '.'
 
 describe('<TextField />', () => {
-  it('Renders with Label', () => {
+  it('should render with Label', () => {
     render(<TextField label="Label" name="Label" />)
-
     expect(screen.getByLabelText('Label')).toBeInTheDocument()
   })
 
-  it('Renders without Label', () => {
+  it('should render without Label', () => {
     render(<TextField />)
-
     expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
   })
 
-  it('Renders with placeholder', () => {
+  it('should render with placeholder', () => {
     render(<TextField placeholder="hey you" />)
-
     expect(screen.getByPlaceholderText('hey you')).toBeInTheDocument()
   })
 
-  it('Changes its value when typing', async () => {
+  it('should change its value when typing', async () => {
     const onInputChange = jest.fn()
     render(
       <TextField
@@ -41,48 +38,5 @@ describe('<TextField />', () => {
       expect(onInputChange).toHaveBeenCalledTimes(text.length)
     })
     expect(onInputChange).toHaveBeenCalledWith(text)
-  })
-
-  it('Does not changes its value when disabled', async () => {
-    const onInputChange = jest.fn()
-    render(
-      <TextField
-        onInputChange={onInputChange}
-        label="TextField"
-        name="TextField"
-        disabled
-      />
-    )
-
-    const input = screen.getByRole('textbox')
-    expect(input).toBeDisabled()
-
-    const text = 'This is my new text'
-    userEvent.type(input, text)
-
-    await waitFor(() => {
-      expect(input).not.toHaveValue(text)
-    })
-    expect(onInputChange).not.toHaveBeenCalled()
-  })
-
-  it('Is accessible by tab', () => {
-    render(<TextField label="TextField" name="TextField" />)
-
-    const input = screen.getByLabelText('TextField')
-    expect(document.body).toHaveFocus()
-
-    userEvent.tab()
-    expect(input).toHaveFocus()
-  })
-
-  it('Is not accessible by tab when disabled', () => {
-    render(<TextField label="TextField" name="TextField" disabled />)
-
-    const input = screen.getByLabelText('TextField')
-    expect(document.body).toHaveFocus()
-
-    userEvent.tab()
-    expect(input).not.toHaveFocus()
   })
 })
